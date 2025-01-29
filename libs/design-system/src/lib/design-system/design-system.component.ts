821732d70cc690ad17819e17f0d0ce73b1384d7d
Kamil Korczynski <korczas@gmail.com>
2025-01-29 15:35:45 +0100

    feat: bootstrap design-system

---
+++ libs/design-system/src/lib/design-system/design-system.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'lib-design-system',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './design-system.component.html',
+    styleUrl: './design-system.component.scss',
+})
+export class DesignSystemComponent {}

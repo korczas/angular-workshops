5a9d5615e0e70df616726d634c9466b57df206c8
Kamil Korczynski <korczas@gmail.com>
2025-02-05 14:21:08 +0100

    feat: add FormCheckbox component

--- libs/design-system/src/index.ts
+++ libs/design-system/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/form-input/form-input.component';
 export * from './lib/form-textarea/form-textarea.component';
 export * from './lib/form-select/form-select.component';
 export * from './lib/form-datepicker/form-datepicker.component';
+export * from './lib/form-checkbox/form-checkbox.component';
